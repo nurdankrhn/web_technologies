64b9b7fe3c4573614bf909cebe45d1140dbd4efa
Nurdan KARAHAN <nurdankarahan001@gmail.com>
2023-12-13 13:38:43 +0300

    some picture remove

---
+++ atama.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$telefon=$_POST['telefon'];
+$mesaj=$_POST['mesaj'];
+
+
+
+echo "Hoşgeldiniz Sayın : ".$name.'<br>'."Telefon Numaranız : ".$telefon.'<br>'."Mesajınız : ".$mesaj.'<br>'."Bilgileriniz alınmıştır..." ;
+
+
+?>
